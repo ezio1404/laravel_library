e604ec85888da7680c75adb8874241dcdd7babeb
EJ Anton Potot <zuming1404@gmail.com>
2020-10-09 14:38:37 +0800

    Added Book routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/books', 'BooksController@store');
+Route::patch('/books/{book}', 'BooksController@update');
