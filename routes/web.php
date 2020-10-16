2fed634ff1a270947c732ecd1e113dda4cc0adc1
EJ Anton Potot <zuming1404@gmail.com>
2020-10-16 15:55:32 +0800

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::post('/books', 'BooksController@store');
 Route::patch('/books/{book}', 'BooksController@update');
 Route::delete('/books/{book}', 'BooksController@destroy');
 
-Route::post('/author', 'AuthorsController@store');
+Route::post('/authors', 'AuthorsController@store');
